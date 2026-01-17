5e2e443b0b0121d2cc733249d8960c68049a5e8c
abednego emonena <emonenaabednego@gmail.com>
2026-01-17 10:07:42 +0100

    fix: Update BaseUrl in AuthContext and page to use the correct API endpoint

--- lensra-frontend/app/page.tsx
+++ lensra-frontend/app/page.tsx
@@ -9,7 +9,7 @@ import {
   Shirt, Coffee, Home, Briefcase, Gift, Sparkles, Clock, MapPin, Eye
 } from 'lucide-react';
 
-const BaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000/";
+const BaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://api.lensra.com/";
 
 interface Product {
   id: number;
--- lensra-frontend/context/AuthContext.tsx
+++ lensra-frontend/context/AuthContext.tsx
@@ -36,7 +36,7 @@ type AuthContextType = {
   logout: () => Promise<void>;
 };
 
-const BaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000/";
+const BaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://api.lensra.com/";
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
