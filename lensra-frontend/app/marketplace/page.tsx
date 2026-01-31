c0590b6d91ed040f9b9d6c8fdc94fece2c443603
abednego emonena <emonenaabednego@gmail.com>
2026-01-31 13:02:33 +0100

    style: Add console log for fetched products in Marketplace component

--- lensra-frontend/app/marketplace/page.tsx
+++ lensra-frontend/app/marketplace/page.tsx
@@ -24,6 +24,7 @@ async function getProducts() {
 
 export default async function Marketplace() {
   const products = await getProducts();
+  console.log('Fetched products:', products);
 
   return <MarketplacePage initialProducts={products} />;
 }
